95658e66b9414607d4b9d9fd772a30ff3866e679
luke2327 <qwepoi8526@gmail.com>
2018-04-06 19:56:33 +0900

    Login and Sign up add but is not Working.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^join/$', views.signup, name='join'),
+    url(r'^login/$', views.signin, name='login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
-from django.shortcuts import redirect
+from .forms import PostForm, UserForm, LoginForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.template import RequestContext
+from django.http import HttpResponse
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -36,3 +39,30 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('post_list')
+    else:
+        form = UserForm()
+        return render(request, 'blog/adduser.html', {'form': form})
+
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            # return HttpResponse(request.POST['username'])
+            return render(request, 'blog/post_list.html', {'form': form})
+    else:
+        form = LoginForm()
+        return render(request, 'blog/login.html', {'form': form})
